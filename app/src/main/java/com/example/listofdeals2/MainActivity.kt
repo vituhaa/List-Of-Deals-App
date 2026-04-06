d7d60242a55c632642bff6ac34c78a2a86980ed2
vituhaa <vbuzina72@gmail.com>
2026-04-06 18:16:17 +0300

    code has been structured

--- app/src/main/java/com/example/listofdeals2/MainActivity.kt
+++ app/src/main/java/com/example/listofdeals2/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.listofdeals2
 
-import android.R
+//import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -19,6 +19,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,6 +32,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -38,6 +42,7 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import com.example.listofdeals2.ui.theme.ListOfDeals2Theme
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,57 +55,182 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ListOfDeals() {
-    val dealList = listOf(
-        "Погулять с собакой", "Помыть посуду", "Сделать уроки", "Убраться в комнате"
+fun Header() {
+    Text(
+        text = "Список дел",
+        modifier = Modifier.padding(32.dp),
+        style = TextStyle(
+            color = Color.Black,
+            fontSize = TextUnit(value = 24f, type = TextUnitType.Sp)
+        ),
+        fontWeight = FontWeight.ExtraBold
     )
+}
 
-    var isCheckedState by remember {
-        mutableStateOf(dealList.associateWith { false }.toMutableMap())
+@Composable
+fun AddDeal() {
+    var newDeal by remember {
+        mutableStateOf("")
     }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Список дел",
-            modifier = Modifier.padding(32.dp),
-            style = TextStyle(
-                color = Color.Black,
-                fontSize = TextUnit(value = 24f, type = TextUnitType.Sp)
-            ),
-            fontWeight = FontWeight.ExtraBold
-        )
-        LazyColumn {
-            items(dealList) { deal ->
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(8.dp)
-                ) {
-                    Checkbox(
-                        checked = isCheckedState.getValue(deal),
-                        onCheckedChange = { isChecked ->
-                            isCheckedState = isCheckedState.toMutableMap().apply {
-                                put(deal, isChecked)
-                            }
-                        }
-                    )
-                    Text(
-                        text = deal,
-                        modifier = Modifier.padding(8.dp)
-                    )
-                }
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(5.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    )
+    {
+        OutlinedTextField(
+            modifier = Modifier.weight(1f),
+            value = newDeal,
+            onValueChange = {
+                newDeal = it
             }
+        )
+        Button(onClick = {}) {
+            Text(text = "Add deal")
         }
     }
 }
 
+@Composable
+fun ListOfDeals() {
+    val dealList = listOf(
+        "Погулять с собакой", "Помыть посуду", "Сделать уроки", "Убраться в комнате"
+    )
+    LazyColumn {
+        items(dealList) { deal ->
+            CheckBoxDeals(deal = deal)
+        }
+    }
+}
+
+@Composable
+fun CheckBoxDeals(deal: String) {
+    var isCheckedState by remember {
+        mutableStateOf(false)
+    }
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = isCheckedState,
+            onCheckedChange = { isCheckedState = it },
+            colors = CheckboxDefaults.colors(
+                checkedColor = Color.Green,
+                uncheckedColor = Color.DarkGray,
+                checkmarkColor = Color.White
+            )
+        )
+        Text(
+            text = deal,
+            modifier = Modifier.weight(1f) // выстраивает строки задач красиво
+        )
+        IconButton(onClick = {}) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_delete_outline_24),
+                contentDescription = "Delete deal"
+            )
+        }
+    }
+}
+
+@Composable
+fun dealListApp() {
+    var isCheckedState by remember {
+        mutableStateOf(false)
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Header()
+        AddDeal()
+        ListOfDeals()
+    }
+}
+
+//@Composable
+//fun ListOfDeals() {
+//    val dealList = listOf(
+//        "Погулять с собакой", "Помыть посуду", "Сделать уроки", "Убраться в комнате"
+//    )
+//
+//    var isCheckedState by remember {
+//        mutableStateOf(dealList.associateWith { false }.toMutableMap())
+//    }
+//
+//    var newDeal by remember {
+//        mutableStateOf("")
+//    }
+//
+//    Column(
+//        modifier = Modifier.fillMaxSize(),
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        Text(
+//            text = "Список дел",
+//            modifier = Modifier.padding(32.dp),
+//            style = TextStyle(
+//                color = Color.Black,
+//                fontSize = TextUnit(value = 24f, type = TextUnitType.Sp)
+//            ),
+//            fontWeight = FontWeight.ExtraBold
+//        )
+//        Row(
+//            modifier = Modifier.fillMaxWidth()
+//                .padding(5.dp),
+//            horizontalArrangement = Arrangement.SpaceEvenly
+//        )
+//        {
+//            OutlinedTextField(
+//                modifier = Modifier.weight(1f),
+//                value = newDeal,
+//                onValueChange = {
+//                    newDeal = it
+//            })
+//            Button(onClick = {}) {
+//                Text(text = "Add deal")
+//            }
+//        }
+//        LazyColumn {
+//            items(dealList) { deal ->
+//                Row(
+//                    verticalAlignment = Alignment.CenterVertically,
+//                    modifier = Modifier.padding(8.dp)
+//                ) {
+//                    Checkbox(
+//                        checked = isCheckedState.getValue(deal),
+//                        onCheckedChange = { isChecked ->
+//                            isCheckedState = isCheckedState.toMutableMap().apply {
+//                                put(deal, isChecked)
+//                            }
+//                        },
+//                        colors = CheckboxDefaults.colors(
+//                            checkedColor = Color.Green,
+//                            uncheckedColor = Color.DarkGray,
+//                            checkmarkColor = Color.White
+//                        )
+//                    )
+//                    Text(
+//                        text = deal,
+//                        modifier = Modifier.weight(1f) // выстраивает строки задач красиво
+//                    )
+//                    IconButton(onClick = {}) {
+//                        Icon(
+//                            painter = painterResource(id = R.drawable.baseline_delete_outline_24),
+//                            contentDescription = "Delete deal"
+//                        )
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable
 fun ListOfDealsPreview() {
     ListOfDeals2Theme {
-        ListOfDeals()
+        dealListApp()
     }
 }
