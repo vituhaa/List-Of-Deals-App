6f355c0d75a6d01a8bcac150c363770eb717e9b1
vituhaa <vbuzina72@gmail.com>
2026-04-06 18:53:02 +0300

    fixed

--- app/src/main/java/com/example/listofdeals2/MainActivity.kt
+++ app/src/main/java/com/example/listofdeals2/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -22,8 +24,11 @@ import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
@@ -79,11 +84,16 @@ fun AddDeal() {
     )
     {
         OutlinedTextField(
-            modifier = Modifier.weight(1f),
             value = newDeal,
             onValueChange = {
                 newDeal = it
-            }
+            },
+            modifier = Modifier.width(250.dp)
+                               .height(55.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedTextColor = Color.Black,
+                unfocusedTextColor = Color.Black
+            )
         )
         Button(onClick = {}) {
             Text(text = "Add deal")
@@ -135,7 +145,7 @@ fun CheckBoxDeals(deal: String) {
 }
 
 @Composable
-fun dealListApp() {
+fun DealListApp() {
     var isCheckedState by remember {
         mutableStateOf(false)
     }
@@ -231,6 +241,6 @@ fun dealListApp() {
 @Composable
 fun ListOfDealsPreview() {
     ListOfDeals2Theme {
-        dealListApp()
+        DealListApp()
     }
 }
